a936b03176ec9f73625259631306bfbe9cc199ee
Taimoor Zaeem <93754271+radiocoder@users.noreply.github.com>
2021-11-06 08:03:47 +0000

    Added the tas3-1.cpp and task3-2.cpp files.

---
+++ task3-1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+  
+bool moveMin(std::vector<int> &in, std::vector<int> &out) {
+    int i = 0, j = 0;
+
+    for (i = 0; i < in.size(); i++) {
+        out.push_back(in[i]);
+    }
+
+    for (i = out.size() - 1; i > 0; i--) {
+        if(out[i] < out[i-1]) {
+            int temp = out[i];
+            out[i] = out[i-1];
+            out[i-1] = temp;
+        }
+    }
+    return true;
+}
+
+bool testMoveMin(int size) {
+    int i = 0, j = 0;
+    std::vector<int> input;
+    std::vector<int> output;
+    
+    for (i = 0; i < size; i++) {
+        input.push_back(rand() % 100 + 1);
+    }
+
+    sort(input.begin(), input.end());
+    input.push_back(23);
+ 
+    moveMin(input, output);
+
+    sort(input.begin(), input.end());
+
+    for (i = 0; i < input.size(); i++) {
+        if (input[i] != output[i]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> numbers = { 3, 5, 12, 24, 25, 27, 15 };
+    std::vector<int> out;
+    bool rc = moveMin(numbers, out);
+    
+    int i = 0;
+
+    rc = testMoveMin(10);
+    rc = testMoveMin(100);
+    rc = testMoveMin(1000);
+    rc = testMoveMin(10000);
+
+    auto start = high_resolution_clock::now();
+    
+    rc = testMoveMin(100000);
+    
+    auto stop = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop - start);
+    
+    std::cout << (duration1.count() * 100) / 1000000 << " seconds for 100 cases" << std::endl;
+
+    return 0;
+}
+
---
+++ task3-2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+using namespace std::chrono;
+
+
+bool bubbleSort(std::vector<int> &in, std::vector<int> &out) {
+    int i = 0, j = 0;
+
+    for (i = 0; i < in.size(); i++) {
+        out.push_back(in[i]);
+    }
+
+    for (i = 0; i < out.size() - 1; i++) {
+        for (j = 0; j < out.size() - i - 1; j++) {
+            if (out[j] > out[j+1]) {
+                int temp = out[j];
+                out[j] = out[j+1];
+                out[j+1] = temp;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool testBubbleSort(int size) {
+    int i = 0, j = 0;
+    std::vector<int> input;
+    std::vector<int> output;
+    
+    for (i = 0; i < size; i++) {
+        input.push_back(rand() % 100 + 1);
+    }
+    sort(input.begin(), input.end());
+    input.push_back(23);
+ 
+    bubbleSort(input, output);
+
+    sort(input.begin(), input.end());
+
+    for (i = 0; i < input.size(); i++) {
+        if (input[i] != output[i]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> numbers = { 3, 5, 12, 24, 25, 27, 15 };
+    std::vector<int> out;
+    
+    int i = 0;
+
+    bool rc = testBubbleSort(10);
+    rc = testBubbleSort(100);
+    rc = testBubbleSort(1000);
+    rc = testBubbleSort(10000);
+
+    std::cerr << "Processing...\n";
+    auto start = high_resolution_clock::now();
+
+    rc = testBubbleSort(100000);
+
+    auto stop = high_resolution_clock::now();
+
+    auto duration2 = duration_cast<microseconds>(stop - start);
+  
+    std::cerr << (double)((duration2.count() * 100) / 1000000) << " seconds for 100 cases" << "\n";
+    return 0;
+}
